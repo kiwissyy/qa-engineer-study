41ad0ef52397b949b4cf1f4bd6bc26d1f49b1010
kiwissyy <devssyoon@gmail.com>
2025-03-26 11:52:43 +0900

    공통 훅 추가

--- cypressStudy/learn-next-page-router/cypress/support/commands.js
+++ cypressStudy/learn-next-page-router/cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// 커스텀 커맨드 등록
+
+/**
+ * @description data-cy로 설정된 엘리먼트를 쉽게 찾을 수 있는 커스텀 API
+ */
+Cypress.Commands.add('getByCy', text => {
+	return cy.get(`[data-cy=${text}]`);
+});
